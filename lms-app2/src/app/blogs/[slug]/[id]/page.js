2d6b14eb0e666c7f7fc41d14b814a6e2291a62cb
loredonrj <loredonrj@users.noreply.github.com>
2023-06-13 01:34:12 +0000

    clean

---
+++ lms-app2/src/app/blogs/[slug]/[id]/page.js
@@ -0,0 +1,3 @@
+export default function BlogInnerDetails(){
+    return <h1>Blog Post BlogInnerDetails page</h1>
+}
---
+++ lms-app2/src/app/contact/page.js
@@ -0,0 +1,3 @@
+export default function Contact(){
+    return <h1>Contact page body (/contact route or path)</h1>
+}
